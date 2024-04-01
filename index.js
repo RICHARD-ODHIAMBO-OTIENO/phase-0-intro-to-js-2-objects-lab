33803fdb8b1e1f7506627acfe1c70c51f19f3dc5
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-04-01 22:17:12 +0300

    Intro-to-js-2-objects-lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+let employee = {
+    name : 'Ken',
+    streetAddress : 'Ralph Road'
+};
+
+// create a function that update employee with key and value 
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value};
+}
+
+// create a function that destructively update employee with key and value
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+// create a fucntion that deletes from employee by key
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+};
+
+// create a function that destructively delete from employee by key 
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
